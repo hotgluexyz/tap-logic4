3e2c79eeba06fee9bd9f0426cb0162e00c64718a
José Arilton Pereira Filho <arilton@hotglue.io>
2024-10-08 16:58:07 +0200

    fix pagination issue (#17)
    Co-authored-by: Hassan Syyid <hassansyyid@gmail.com>

--- tap_logic4/streams.py
+++ tap_logic4/streams.py
@@ -194,11 +194,12 @@ class ProductsStream(Logic4Stream):
         th.Property("IsVisibleInLogic4", th.BooleanType),
     ).to_dict()
     
+    # we need to go over each pair of IsVisibleOnWebShop and IsVisibleInLogic4
     is_visible_pairs = [
-        {"IsVisibleOnWebShop": False, "IsVisibleInLogic4": True},
-        {"IsVisibleOnWebShop": False, "IsVisibleInLogic4": False},
         {"IsVisibleOnWebShop": True, "IsVisibleInLogic4": True},
+        {"IsVisibleOnWebShop": False, "IsVisibleInLogic4": True},
         {"IsVisibleOnWebShop": True, "IsVisibleInLogic4": False},
+        {"IsVisibleOnWebShop": False, "IsVisibleInLogic4": False},
     ]
     is_visible_current_pair = None
     
@@ -206,11 +207,13 @@ class ProductsStream(Logic4Stream):
         payload = super().prepare_request_payload(context, None)
         if next_page_token and next_page_token.get("counter"):
             payload["SkipRecords"] = next_page_token.get("counter")
-        if (self.is_visible_pairs and next_page_token and next_page_token.get("counter") == 0) or \
-            (len(self.is_visible_pairs) == 4):  # if is the first attempt
-            self.is_visible_current_pair = self.is_visible_pairs.pop()
-        else:
-            self.is_visible_current_pair = None
+        
+        # we need to change the pairs if is the first attempt or the response doesn't have any record (counter=0)
+        if (len(self.is_visible_pairs) == 4) or (next_page_token and next_page_token.get("counter") == 0):
+            if self.is_visible_pairs:
+                self.is_visible_current_pair = self.is_visible_pairs.pop()
+            else:
+                self.is_visible_current_pair = None
         if self.is_visible_current_pair:
             payload.update(self.is_visible_current_pair)
             context.update(self.is_visible_current_pair)
@@ -221,16 +224,16 @@ class ProductsStream(Logic4Stream):
     
     def get_next_page_token(self, response, previous_token):
         counter = response.json().get("RecordsCounter")
+        if counter == 0 and len(self.is_visible_pairs) == 0:
+            return None  # this mean we go over all pages through all pairs
+        
         next_page_token = {"counter": 0}
-
         if counter:
-            previous_token = previous_token.get("counter", 0) if previous_token else 0
-            next_page_token["counter"] = previous_token + counter
+            prev_token = previous_token.get("counter", 0) if previous_token else 0
+            next_page_token["counter"] = prev_token + counter
         if self.is_visible_current_pair:
             next_page_token.update(self.is_visible_current_pair)
         
-        if not counter and self.is_visible_current_pair is None:
-            return None
         return next_page_token
     
     def post_process(self, row, context):
